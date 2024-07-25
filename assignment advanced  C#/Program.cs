d9fd8d052569ddb344a5736a53f84015af18545d
mo7medelgabry <mo7medelgabry@gmail.com>
2024-07-25 03:55:22 +0300

    Add project files.

---
+++ assignment advanced  C#/Program.cs
@@ -0,0 +1,64 @@
+﻿using assignment_advanced__C_.genarics;
+using System;
+
+namespace assignment_advanced__C_
+{
+    internal class Program
+
+    {
+        //#region 1
+        //static void Main()
+        //{
+        //    int[] array = { 99,30,7,10,3,50};
+        //    sorting(array);
+        //    Console.WriteLine( string.Join(", ", array));
+        //}
+        //static void  sorting(int[] arr)
+        //{
+        //    int n = arr.Length;
+        //    for (int i = 0; i < n - 1; i++)
+        //    {
+        //        bool swapped = false;
+
+        //        for (int j = 0; j < n - i - 1; j++)
+        //        {
+        //            if (arr[j] > arr[j + 1])
+        //            {
+
+        //                int temp = arr[j];
+        //                arr[j] = arr[j + 1];
+        //                arr[j + 1] = temp;
+
+
+        //                swapped = true;
+        //            }
+        //        }
+        //        if (!swapped)
+        //        {
+        //            break;
+        //        }
+        //    }
+        //}
+        //#endregion
+
+        #region 2
+        public static void Main(string[] args)
+        {
+            Range<int> intRange = new Range<int>(2, 20);
+            Console.WriteLine(  intRange.IsInRange(10));
+            Console.WriteLine( intRange.Length());
+
+            Range<double> doubleRange = new Range<double>(3.5, 12.4);
+            Console.WriteLine( doubleRange.IsInRange(15));
+            Console.WriteLine( doubleRange.Length());
+
+        }
+        #endregion
+
+
+
+
+
+
+    }
+}
---
+++ assignment advanced  C#/genarics/Range.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_advanced__C_.genarics
+{
+    public class Range<T> where T:IComparable<T>
+    {
+        public T Min { get; private set; }
+        public T Max { get; private set; }
+        public Range(T min, T max)
+        {
+            if (min.CompareTo(max) > 0)
+            {
+                throw new ArgumentException("invalid");
+            }
+
+            Min = min;
+            Max = max;
+        }
+        public bool IsInRange(T value)
+        {
+            return value.CompareTo(Min) >= 0 && value.CompareTo(Max) <= 0;
+        }
+
+        public T Length()
+        {
+
+            dynamic min = Min;
+            dynamic max = Max;
+            return max - min;
+        }
+    }
+
+}
+
